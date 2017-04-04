7d43b2b6b6955963ee492813cd7f7a9f6c59ffeb
Dan 'Ducky' Little <theduckylittle@gmail.com>
2017-04-04 13:45:24 -0500

    update copyright

--- php/download.php
+++ php/download.php
@@ -1,5 +1,5 @@
 <?php
-/*Copyright (c) 2009, Dan "Ducky" Little & GeoMOOSE.org
+/*Copyright (c) 2009, 2017, Dan "Ducky" Little & GeoMOOSE.org
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
